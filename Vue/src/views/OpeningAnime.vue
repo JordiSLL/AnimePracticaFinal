75eb24da4cd1dde343127d028cb87fe4d64dd645
Jordi Sanchez <jsanchezlloansi@gmail.com>
2023-06-16 17:43:41 +0200

    OpeningAnime v2

--- Vue/src/views/OpeningAnime.vue
+++ Vue/src/views/OpeningAnime.vue
@@ -8,7 +8,11 @@ mounted(){
 methods:{
   sendRequest(){
     randomOpService.getRandomOpening().then( data => {
+      let random = Math.floor(Math.random() * data.data.anime.animethemes.length);
       console.log(data)
+      let songLink = data.data.anime.animethemes[random].animethemeentries[0].videos[0].audio.link
+      console.log(songLink);
+      document.querySelector('audio').src = songLink;
     })
   },
   validation(){
@@ -25,13 +29,15 @@ methods:{
 <template>
   <main>
     <h1>Guess the Random Anime</h1>
+    <h2 class="animeName">AnimeName:</h2>
     <audio controls>
-  <source src="" type="audio/mp3">
+  <source src="" type="audio/ogg">
   Tu navegador no soporta el elemento de audio.
 </audio>
+<input class="text" type="text">
 <button @click="sendRequest">RandomOpening</button>
-<button @click="validation">I got it</button>
-<button @click="surrender">Wtf is this opening</button>
+<button @click="validation" class="btnValidation">I got it</button>
+<button @click="surrender" class="btnSurrender">Wtf is this opening</button>
   </main>
 </template>
 
@@ -51,7 +57,15 @@ button {
   background: rgb(68, 29, 177);
   background: linear-gradient(0deg, rgb(68, 29, 177) 0%, rgba(129,5,240,1) 100%);
   border: none;
+  margin: 1rem;
 }
+.btnValidation{
+  background: rgb(27, 208, 39);
+}
+.btnSurrender{
+  background: rgb(198, 23, 23);
+}
+
 button:hover {
   box-shadow:  4px 4px 6px 0 rgba(255,255,255,.5),
               -4px -4px 6px 0 rgba(116, 125, 136, .5), 
