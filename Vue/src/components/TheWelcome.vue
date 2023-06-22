5d04a999b599d3f1a646a3eab9a13b9d138357f8
JordiSLL <jsanchezlloansi@gmail.com>
2023-06-22 21:46:42 +0200

    Home Done

--- Vue/src/components/TheWelcome.vue
+++ Vue/src/components/TheWelcome.vue
@@ -14,26 +14,22 @@ import SupportIcon from './icons/IconSupport.vue'
     </template>
     <template #heading>Documentation</template>
 
-    Vue’s
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
-    provides you with all information you need to get started.
+    AnimesRandom 
+    <a href="https://github.com/JordiSLL/AnimePracticaFinal" target="_blank" rel="noopener">official Github</a>
+    
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon>
       <ToolingIcon />
     </template>
-    <template #heading>Tooling</template>
+    <template #heading>Functionalities</template>
 
-    This project is served and bundled with
-    <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The
-    recommended IDE setup is
-    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a> +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener">Volar</a>. If
-    you need to test your components and web pages, check out
-    <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a> and
-    <a href="https://on.cypress.io/component" target="_blank">Cypress Component Testing</a>.
-
+    Descubre nuevos animes en base al type, score, rating, status o genres.<br />
+    Adivina el Op/Ed de animes aleatorios.<br />
+    Esta a aplicacion utiliza llamadas a dos Apis: 
+    <a href="https://jikan.moe/" target="_blank" rel="noopener"> Jikan.moe</a>,
+    <a href="https://animethemes.moe/" target="_blank" rel="noopener"> animethemes.moe</a>
     <br />
 
     More instructions are available in <code>README.md</code>.
@@ -43,34 +39,22 @@ import SupportIcon from './icons/IconSupport.vue'
     <template #icon>
       <EcosystemIcon />
     </template>
-    <template #heading>Ecosystem</template>
+    <template #heading>Build With</template>
 
-    Get official tools and libraries for your project:
-    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
-    <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
-    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener">Vue Test Utils</a>, and
-    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>. If
-    you need more resources, we suggest paying
-    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
-    a visit.
+    Este proyecto se ha construido con: 
+    <a href="https://vuejs.org/" target="_blank" rel="noopener"> VueJS</a>,
+    <a href="https://nodejs.org/" target="_blank" rel="noopener"> Node JS</a>
+    
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon>
       <CommunityIcon />
     </template>
-    <template #heading>Community</template>
+    <template #heading>GlobalChat</template>
 
-    Got stuck? Ask your question on
-    <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>, our official
-    Discord server, or
-    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
-      >StackOverflow</a
-    >. You should also subscribe to
-    <a href="https://news.vuejs.org" target="_blank" rel="noopener">our mailing list</a> and follow
-    the official
-    <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
-    twitter account for latest news in the Vue world.
+      Interactua con otros usarios en el chat global. Muy Pronto Nuevas funcionalidades...
+    
   </WelcomeItem>
 
   <WelcomeItem>
@@ -78,9 +62,6 @@ import SupportIcon from './icons/IconSupport.vue'
       <SupportIcon />
     </template>
     <template #heading>Support Vue</template>
-
-    As an independent project, Vue relies on community backing for its sustainability. You can help
-    us by
-    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
+    Hecho por Jordi Sánchez y Marc Galobart.
   </WelcomeItem>
 </template>
